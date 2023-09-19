ea81468c581cc58a296d364e64cbbfdb840c430b
suddii01 <suddii01@naver.com>
2023-09-19 16:11:08 +0900

    feat(P2): 데이터 유효성 검증 로직 구현

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,15 +1,38 @@
 package onboarding;
 
+import onboarding.problem2.Problem2Exception;
+import onboarding.problem2.Problem2ExceptionType;
+
 import java.util.ArrayList;
 import java.util.List;
+
+import static onboarding.problem2.Problem2Constant.*;
 
 public class Problem2 {
 
     public static String solution(String cryptogram) {
+        try {
+            validateCryptogram(cryptogram);
+        } catch (Problem2Exception e) {
+            System.err.println("Error Type: " + e.getType());
+            System.err.println("Error Message: " + e.getMessage());
+        }
+
         while (hasDuplicatedChars(cryptogram)) {
             cryptogram = removeDuplicatedChars(cryptogram);
         }
         return cryptogram;
+    }
+
+    public static void validateCryptogram(String cryptogram) throws Problem2Exception {
+        if (cryptogram.length() < CRYPTOGRAM_MIN_LENGTH || cryptogram.length() > CRYPTOGRAM_MAX_LENGTH) {
+            throw new Problem2Exception(Problem2ExceptionType.INVALID_STRING_LENGTH);
+        }
+        for (int i = 0; i < cryptogram.length(); i++) {
+            if (cryptogram.charAt(i) < FIRST_LOWER_ALPHABET || cryptogram.charAt(i) > LAST_LOWER_ALPHABET) {
+                throw new Problem2Exception(Problem2ExceptionType.INVALID_CHARACTER);
+            }
+        }
     }
 
     public static boolean hasDuplicatedChars(String cryptogram) {
