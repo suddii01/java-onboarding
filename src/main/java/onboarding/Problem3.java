a175c3467d66a77a6628752bded578a26eecf5c4
suddii01 <suddii01@naver.com>
2023-09-19 16:40:46 +0900

    feat(P3): 기능 구현

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -2,7 +2,26 @@ package onboarding;
 
 public class Problem3 {
     public static int solution(int number) {
-        int answer = 0;
-        return answer;
+        return getTotalClapCount(number);
+    }
+
+    public static int getTotalClapCount(int number) {
+        int totalClapCnt = 0;
+        for (int i = 1; i <= number; i++) {
+            totalClapCnt += getClapCount(i);
+        }
+        return totalClapCnt;
+    }
+
+    public static int getClapCount(int number) {
+        Integer integer = Integer.valueOf(number);
+        String s = integer.toString();
+        int clapCnt = 0;
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == '3') clapCnt++;
+            if (s.charAt(i) == '6') clapCnt++;
+            if (s.charAt(i) == '9') clapCnt++;
+        }
+        return clapCnt;
     }
 }
