2079d1394fd25f4a4df1b2478afa6053bd2005dd
suddii01 <suddii01@naver.com>
2023-09-19 18:31:10 +0900

    feat(P5): 기능 구현

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -1,11 +1,34 @@
 package onboarding;
 
-import java.util.Collections;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+
+import static onboarding.problem5.Problem5Constant.*;
 
 public class Problem5 {
     public static List<Integer> solution(int money) {
-        List<Integer> answer = Collections.emptyList();
-        return answer;
+        return convertMoneyToList(money);
+    }
+
+    public static List<Integer> convertMoneyToList(int money) {
+        List<Integer> currencyUnits = Arrays.asList(FIFTY_THOUSAND_WON, TEN_THOUSAND_WON, FIVE_THOUSAND_WON, ONE_THOUSAND_WON, FIVE_HUNDRED_WON, ONE_HUNDRED_WON, FIFTY_WON, TEN_WON, ONE_WON);
+        List<Integer> currencyCnt = initCurrencyCnt(currencyUnits.size());
+        for (int i = 0; i < currencyUnits.size(); i++) {
+            Integer currencyUnit = currencyUnits.get(i);
+            if (money / currencyUnit > 0) {
+                currencyCnt.set(i, money / currencyUnit);
+                money %= currencyUnit;
+            }
+        }
+        return currencyCnt;
+    }
+
+    public static List<Integer> initCurrencyCnt(int size) {
+        List<Integer> currencyCnt = new ArrayList<>();
+        for (int i = 0; i < size; i++) {
+            currencyCnt.add(0);
+        }
+        return currencyCnt;
     }
 }
---
+++ src/main/java/onboarding/problem5/Problem5Constant.java
@@ -0,0 +1,15 @@
+package onboarding.problem5;
+
+public class Problem5Constant {
+    public static final int FIFTY_THOUSAND_WON = 50000;
+    public static final int TEN_THOUSAND_WON = 10000;
+    public static final int FIVE_THOUSAND_WON = 5000;
+    public static final int ONE_THOUSAND_WON = 1000;
+    public static final int FIVE_HUNDRED_WON = 500;
+    public static final int ONE_HUNDRED_WON = 100;
+    public static final int FIFTY_WON = 50;
+    public static final int TEN_WON = 10;
+    public static final int ONE_WON = 1;
+    public static final int MONEY_MIN_SIZE = 1;
+    public static final int MONEY_MAX_SIZE = 1000000;
+}
